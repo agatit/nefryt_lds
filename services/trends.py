700840e44bac953c64b0133d8c4f82e5b801dd71
karqz <karqkolu@gmail.com>
2021-07-29 15:50:08 +0200

    Szkic trends i trends_writer

--- services/trends.py
+++ services/trends.py
@@ -1,14 +1,14 @@
-import threading
-import pyodbc
 import time
 
 #KAROL: każda klasa ma przynajmniej jeden argument i jest to Trend_ID
 class Trend:
-    def get_ID(self):
-        return int(self.trend_ID)
 
-    def __init__(self, trend_ID):
-        self.trend_ID = trend_ID
+    def get_ID(self):
+        return self.trend_ID
+
+    def __init__(self, server,  trend_ID):
+        self.trend_ID = int(trend_ID)
+        self.server = server
         pass
 
     def run(self):
@@ -17,121 +17,55 @@ class Trend:
 
 class TrendQuick(Trend):
     # powinien uruchamiać wątki serwera modbus
-    def __init__(self, trend_ID, host_name, port, slave_ID,
-                register, raw_min, raw_max, scaled_min, scaled_max ):
-        super().__init__(trend_ID)
+    def __init__(self, server, trend_ID, host_name, port,
+                 slave_ID, register, raw_min,
+                 raw_max, scaled_min, scaled_max ):
+        super().__init__(server, trend_ID)
         self.host_name = host_name
-        self.port = port
-        self.slave_ID = slave_ID
-        self.register = register
-        self.raw_min = raw_min
-        self.raw_max = raw_max
-        self.scaled_min = scaled_min
-        self.scaled_max = scaled_max
+        self.port = int(port)
+        self.slave_ID = int(slave_ID)
+        self.register = int(register)
+        self.raw_min = int(raw_min)
+        self.raw_max = int(raw_max)
+        self.scaled_min = int(scaled_min)
+        self.scaled_max = int(scaled_max)
+        self.data = 100*[0]
+        self.timestamp = time.time()
 
-    def send_data(self):
-        threading.Timer(1, self.send_data).start()
-        #KAROL: wysyła dane do bazy danych, do zrobienia
+    def read(self, slave_id, function_code, address):
+        return self.data[address - self.register]
 
+    def write(self, slave_id, function_code, address, value):
+        self.timestamp = time.time()
+        self.data[address - self.register] = value
 
     def run(self):
-        threading.Timer(1, self.send_data).start()
+        self.server.route_map.add_rule(self.read, [1], [3, 4], list(range(self.register, self.register + 100)))
+        self.server.route_map.add_rule(self.write, [1], [6, 16], list(range(self.register, self.register + 100)))
+        pass
 
     def __str__(self):
-        return str(self.host_name) + ', ' +  str(self.port) + ', ' + str(self.slave_ID) + '...'    
+        return  str(self.trend_ID) + ', ' + str(self.host_name) + ', ' +  str(self.port) + ', ' + str(self.slave_ID) + '...'    
 
 
 class TrendDeriv(Trend):
     # powinien uruchamiać wątek przeliczający, parametrem konstruktora powinien być TrendQuick
     # powinien utrzymywać bufor wartości i uzupełniać ją wartościami z quick trendu
-    def __init__(self, trend_ID, trend_quick, window_size):
-        super().__init__(trend_ID)
+    def __init__(self, server, trend_ID,  trend_quick, window_size):
+        super().__init__(server, trend_ID)
         self.trend_quick = trend_quick
         self.window_size = window_size
 
     def __str__(self):
-        return str(self.trend_ID) + ", " +  str(self.trend_quick)
-
+        return str(self.trend_ID) + ", " +  str(self.trend_quick) + ", " + str(self.window_size)
 
 class TrendMean(Trend):
     # j.w.
-    def __init__(self, trend_ID,  trend_quick, window_size):
-        super().__init__(trend_ID)
+    def __init__(self, server, trend_ID, trend_quick, window_size):
+        super().__init__(server, trend_ID)
         self.trend_quick = trend_quick
         self.window_size = window_size
         pass
 
     def __str__(self):
-        return str(self.trend_ID) + ", " +  str(self.trend_quick)
-
-class TrendWriter:
-
-    #KAROL: zwraca trend o podanym ID, przydatne dla DERIV i MEAN 
-    # bo w bazie danych jest podany nr ID QUICK TREND-U
-    def get_trend(self, trend_ID):
-        for trend in self.trend_list:
-            if trend.get_ID() == int(trend_ID):
-                return trend
-        return None        
-
-
-    def __init__(self, connection_string):
-        # buduje listę obiektów odpowiednich klas na podstawie bazy danych
-        # inicjalizuje je właściwymi parametrami
-        self.trend_list = []
-        trend_list_args = []
-
-        #KAROL: tworzę sobie tablicę z wartościami, następnie je zapisuje i tworzę odpowiednio klasy
-        con = pyodbc.connect(connection_string, unicode_results = True)
-        cur = con.cursor()
-        cur.execute("SELECT \
-                        lds.TrendDef.TrendDefID, \
-                        lds.TrendDefParam.Value, \
-                        lds.TrendDefType.Name \
-                    FROM \
-                    ((  lds.TrendDef \
-                    INNER JOIN lds.TrendDefParam ON (lds.TrendDef.TrendDefID = lds.TrendDefParam.TrendDefID)) \
-                    INNER JOIN lds.TrendDefParamDef ON (lds.TrendDefParam.TrendDefParamDefID = lds.TrendDefParamDef.TrendDefParamDefID)) \
-                    INNER JOIN lds.TrendDefType ON (lds.TrendDefParamDef.TrendDefTypeID = lds.TrendDefType.TrendDefTypeID) \
-                    ORDER BY \
-                    lds.TrendDef.TrendDefID, \
-                    lds.TrendDefParam.TrendDefParamDefID")   
-
-        row = cur.fetchone()
-        while row:
-            args = [row[0]]
-            trend_type = row[2]
-
-            while row and row[0] == args[0]:
-                args.append(row[1])
-                row = cur.fetchone()
-
-            trend_list_args.append([*args, trend_type])       
-        
-        for trend_args in trend_list_args:
-
-            if trend_args[-1] == 'DERIV' :
-                trend = TrendDeriv(trend_args[0], self.get_trend(trend_args[1]), 0)
-
-            elif trend_args[-1] == 'MEAN' : 
-                trend = TrendMean(trend_args[0], self.get_trend(trend_args[1]), 0)
-
-            elif trend_args[-1] == 'QUICK' :
-                trend = TrendQuick(*trend_args[0:9])
-
-            self.trend_list.append(trend) 
-
-        con.close()    
-
-
-    def run(self):
-        for t in self.trend_list:
-            print(t) #t.run() tu będzie 
-
-if __name__ == "__main__":
-
-    tl = TrendWriter('DRIVER={SQL Server};SERVER=SERVERDB\MSSQLSERVER2016' + \
-                    ';DATABASE=NefrytLDS_NEW' + \
-                    ';UID=sa' + \
-                    ';PWD=Onyks$us')
-    tl.run()
+        return str(self.trend_ID) + ", " +  str(self.trend_quick) + ", " + str(self.window_size)
--- services/trends_writer.py
+++ services/trends_writer.py
@@ -1,1 +1,113 @@
-# procedura uruchamiaja wątki
+from os import times
+import pyodbc
+import threading
+import logging
+import time
+import struct
+from datetime import datetime
+from socketserver import ThreadingTCPServer
+from trends import TrendQuick, TrendDeriv, TrendMean
+
+from umodbus import conf
+from umodbus.server.tcp import RequestHandler, get_server
+from umodbus.utils import log_to_stream
+
+log_to_stream(level=logging.DEBUG)
+
+conf.SIGNED_VALUES = True
+
+serv = get_server(ThreadingTCPServer, ("0.0.0.0", 502 ), RequestHandler)
+
+
+def get_time_delay():
+    return int(time.time()) + 1 - time.time()
+
+class TrendWriter:
+
+    #KAROL: zwraca trend o podanym ID, przydatne dla DERIV i MEAN 
+    # bo w bazie danych jest podany nr ID QUICK TREND-U
+    def get_trend(self, trend_ID):
+        for trend in self.trend_list:
+            if trend.get_ID() == int(trend_ID):
+                return trend
+        return None        
+
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+
+
+        # buduje listę obiektów odpowiednich klas na podstawie bazy danych
+        # inicjalizuje je właściwymi parametrami
+        self.trend_list = []
+        trend_list_args = []
+
+        #KAROL: tworzę sobie tablicę z wartościami, następnie je zapisuje i tworzę odpowiednio klasy
+        con = pyodbc.connect(connection_string, unicode_results = True)
+        cur = con.cursor()
+        cur.execute("""SELECT 
+                        lds.TrendDef.TrendDefID, 
+                        lds.TrendDefParam.Value, 
+                        lds.TrendDefType.Name 
+                    FROM 
+                        ((  lds.TrendDef 
+                        INNER JOIN lds.TrendDefParam ON (lds.TrendDef.TrendDefID = lds.TrendDefParam.TrendDefID)) 
+                        INNER JOIN lds.TrendDefParamDef ON (lds.TrendDefParam.TrendDefParamDefID = lds.TrendDefParamDef.TrendDefParamDefID)) 
+                        INNER JOIN lds.TrendDefType ON (lds.TrendDefParamDef.TrendDefTypeID = lds.TrendDefType.TrendDefTypeID) 
+                    ORDER BY 
+                        lds.TrendDef.TrendDefID, 
+                        lds.TrendDefParam.TrendDefParamDefID""")   
+
+        row = cur.fetchone()
+        while row:
+            args = [row[0]]
+            trend_type = row[2]
+
+            while row and row[0] == args[0]:
+                args.append(row[1])
+                row = cur.fetchone()
+
+            trend_list_args.append([*args, trend_type])       
+        
+        for trend_args in trend_list_args:
+
+            if trend_args[-1] == 'DERIV' :
+                trend = TrendDeriv(serv, trend_args[0], self.get_trend(trend_args[1]), trend_args[2])
+
+            elif trend_args[-1] == 'MEAN' : 
+                trend = TrendMean(serv, trend_args[0], self.get_trend(trend_args[1]), trend_args[2])
+
+            elif trend_args[-1] == 'QUICK' :
+                trend = TrendQuick(serv, *trend_args[0:9])
+
+            self.trend_list.append(trend) 
+
+        con.close()    
+
+    def send_data(self, t):
+        threading.Timer(get_time_delay(), self.send_data, args = [t]).start()
+        start_time = round(time.time())
+        timestamp = int(datetime.utcnow().timestamp())
+
+        if start_time - t.timestamp < 1 : 
+            con = pyodbc.connect(self.connection_string, unicode_results = True, autocommit=True)
+            cur = con.cursor()
+            pack = struct.pack('<100H', *t.data)
+            print("Wysyłam paczke z rejestru: " + str(t.register) + 'o godz UTC:' + str(datetime.fromtimestamp(timestamp)))
+            cur.execute("INSERT INTO lds.Trend(TrendDefID, Time, Data) values (?, ?, ?)", t.trend_ID, timestamp, pack)
+            con.close()    
+
+    def run(self):
+        threading.Thread(target = serv.serve_forever).start()
+        for t in self.trend_list: 
+            if (isinstance(t, TrendQuick)):
+                t.run()
+                threading.Timer(get_time_delay(), self.send_data, args = [t]).start()
+       
+
+if __name__ == "__main__":
+
+    tl = TrendWriter('DRIVER={SQL Server};SERVER=SERVERDB\MSSQLSERVER2016' + \
+                    ';DATABASE=NefrytLDS_NEW' + \
+                    ';UID=sa' + \
+                    ';PWD=Onyks$us')
+    tl.run()            
