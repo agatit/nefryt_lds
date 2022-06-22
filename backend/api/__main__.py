f8ad07bd91189849406aa6ab2497fe1400aa8307
hkolodziejski-agatit <hkolodziejski@agatit.pl>
2022-06-22 11:55:51 +0200

    Nowe zapytanie do pobierania danych trendów

--- backend/api/__main__.py
+++ backend/api/__main__.py
@@ -14,16 +14,15 @@ def main():
                 arguments={'title': 'Nefryt LDS API'},
                 pythonic_params=True)
 
-    # app.app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pymssql://sa:Onyks$us@serverdb:1447/NefrytLDSDemo'
-    # app.app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:Onyks$us@serverdb:1447/NefrytLDSDemo?driver=ODBC+Driver+17+for+SQL+Server'
-    app.app.config['SQLALCHEMY_DATABASE_URI'] = \
-        'mssql+pyodbc://sa:Onyks$us@serverdb:1447/NefrytLDSDemo' \
-        '?driver=ODBC+Driver+18+for+SQL+Server' \
-        '&TrustServerCertificate=Yes' \
-        '&Encrypt=No'   
+    app.app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:Onyks$us@serverdb:1447/NefrytLDSDemo?driver=ODBC+Driver+17+for+SQL+Server'
+    # app.app.config['SQLALCHEMY_DATABASE_URI'] = \
+    #     'mssql+pyodbc://sa:Onyks$us@serverdb:1447/NefrytLDSDemo' \
+    #     '?driver=ODBC+Driver+18+for+SQL+Server' \
+    #     '&TrustServerCertificate=Yes' \
+    #     '&Encrypt=No'   
 
     app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"future": True}
+    app.app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"future": True, "echo": False}
 
     db.init_app(app.app)
     CORS(app.app)
