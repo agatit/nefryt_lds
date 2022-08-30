057dcbf14ec3f412ba1dd651d6375b9252308674
pietrzko <pietrzko@o2.pl>
2022-08-30 12:52:56 +0200

    Usuwanie konfliktów c.d.

---
+++ frontend/src/store/emptyApi.ts
@@ -0,0 +1,19 @@
+// Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { AuthData, selectIsAuthenticated } from '../features/auth/authSlice'
+import { RootState } from '../app/store';
+
+
+
+// initialize an empty api service that we'll inject endpoints into later as needed
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://192.168.1.86:8080/',
+  prepareHeaders: (headers, { getState }) => {
+      var token = localStorage.getItem('token');
+      headers.set('Access-Control-Allow-Origin', '*')
+      headers.set('Authorization', `Bearer ${token}`)
+    return headers}
+  }),
+  
+  endpoints: () => ({}),
+})
