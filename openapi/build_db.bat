72a096a6f5c6b4caee0b390f1dd615b31741d950
hkolodziejski-agatit <hkolodziejski@agatit.pl>
2022-11-08 15:00:55 +0100

    Zmiana w generatorze na  --option nojoined

--- openapi/build_db.bat
+++ openapi/build_db.bat
@@ -6,9 +6,9 @@ rem call sqlacodegen.exe --option nobidi --schema editor --outfile ../backend/da
 rem call sqlacodegen.exe --option nobidi --schema obj --outfile ../backend/database/models/obj.py mssql+pyodbc://sa:Onyks$us@serverdb:1447/NefrytLDSDemo?driver=ODBC+Driver+17+for+SQL+Server
 
 
-call sqlacodegen.exe --option nobidi --schema lds  --outfile ../backend/database/models/lds.py mssql+pyodbc://sa:Onyks$us@owl.agatit.pl:1449/NefrytLDS_Zygmuntow_2022?driver=ODBC+Driver+17+for+SQL+Server
-call sqlacodegen.exe --option nobidi --schema editor --outfile ../backend/database/models/editor.py mssql+pyodbc://sa:Onyks$us@owl.agatit.pl:1449/NefrytLDS_Zygmuntow_2022?driver=ODBC+Driver+17+for+SQL+Server
-call sqlacodegen.exe --option nobidi --schema obj --outfile ../backend/database/models/obj.py mssql+pyodbc://sa:Onyks$us@owl.agatit.pl:1449/NefrytLDS_Zygmuntow_2022?driver=ODBC+Driver+17+for+SQL+Server
+call sqlacodegen.exe --option nobidi --option nojoined --schema lds  --outfile ../backend/database/models/lds.py mssql+pyodbc://sa:Onyks$us@owl.agatit.pl:1449/NefrytLDS_Zygmuntow_2022?driver=ODBC+Driver+17+for+SQL+Server
+call sqlacodegen.exe --option nobidi --option nojoined --schema editor --outfile ../backend/database/models/editor.py mssql+pyodbc://sa:Onyks$us@owl.agatit.pl:1449/NefrytLDS_Zygmuntow_2022?driver=ODBC+Driver+17+for+SQL+Server
+call sqlacodegen.exe --option nobidi --option nojoined --schema obj --outfile ../backend/database/models/obj.py mssql+pyodbc://sa:Onyks$us@owl.agatit.pl:1449/NefrytLDS_Zygmuntow_2022?driver=ODBC+Driver+17+for+SQL+Server
 
 
 @echo done
