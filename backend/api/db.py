603f88c842ac5bdf8de3132c3fc494743b01e335
DariuszMielczarek <240752@edu.p.lodz.pl>
2024-09-16 13:36:24 +0200

    Created setup to create and drop database for testing purposes

--- backend/api/db.py
+++ backend/api/db.py
@@ -1,18 +1,14 @@
 from sqlalchemy.orm import Session
 from .config import config
 from sqlalchemy import create_engine, Engine, MetaData
-from database.models.lds import Base as ldsBase
-from database.models.editor import Base as editorBase
 
 
 DATABASE_URL = config.get('db_uri')
 engine = create_engine(url=DATABASE_URL, echo=False)
+test_engine: Engine
 
 
 TEST_DATABASE_URL = config.get('test_db_uri')
-test_engine = create_engine(url=TEST_DATABASE_URL, echo=False)
-ldsBase.metadata.create_all(bind=test_engine)
-editorBase.metadata.create_all(bind=test_engine)
 
 
 def get_engine() -> Engine:
@@ -23,8 +19,12 @@ def get_test_engine() -> Engine:
     return test_engine
 
 
-def clear_test_db():
+def set_test_engine(new_test_engine: Engine):
     global test_engine
+    test_engine = new_test_engine
+
+
+def clear_test_db():
     meta = MetaData()
     meta.reflect(bind=test_engine, schema='editor')
     meta.reflect(bind=test_engine, schema='lds')
--- backend/api/routers/test/conftest.py
+++ backend/api/routers/test/conftest.py
@@ -2,12 +2,52 @@ import logging
 import os
 import sys
 import pytest
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))  # noqa: E402
-from api.db import clear_test_db, test_engine
+from api.config import config
+from api.db import clear_test_db, set_test_engine, get_test_engine
+from database.models.lds import Base as ldsBase
+from database.models.editor import Base as editorBase
 
 
-# has to reset objects manually because of session.refresh() which happens inside of add_events
+SERVER_URL = "mssql+pyodbc://sa:praktyka@localhost/master?driver=ODBC+Driver+17+for+SQL+Server"
+TEST_DATABASE_URL = config.get('test_db_uri')
+TEST_DATABASE_NAME = config.get('test_db_name')
+
+
+# creates new db for testing purposes and drops it after all tests
+@pytest.fixture(scope="session", autouse=True)
+def setup_test_database():
+    engine = create_engine(SERVER_URL)
+    with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
+        result = conn.execute(text(f"SELECT COUNT(*) FROM sys.databases WHERE name = '{TEST_DATABASE_NAME}'"))
+        db_exists = result.scalar() > 0
+
+        if not db_exists:
+            conn.execute(text(f"CREATE DATABASE {TEST_DATABASE_NAME}"))
+            conn.execute(text(f"USE {TEST_DATABASE_NAME}"))
+            conn.execute(text("CREATE SCHEMA lds"))
+            conn.execute(text("CREATE SCHEMA editor"))
+
+    engine.dispose()
+
+    test_engine = create_engine(url=TEST_DATABASE_URL, echo=False)
+    ldsBase.metadata.create_all(bind=test_engine)
+    editorBase.metadata.create_all(bind=test_engine)
+    set_test_engine(test_engine)
+
+    yield
+
+    with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
+        conn.execute(text("USE master"))
+        conn.execute(text(f"ALTER DATABASE {TEST_DATABASE_NAME} SET SINGLE_USER WITH ROLLBACK IMMEDIATE"))
+        conn.execute(text(f"DROP DATABASE {TEST_DATABASE_NAME}"))
+
+    engine.dispose()
+
+
+# has to reset objects manually because of session.refresh() which happens inside of add_lds_objects
 @pytest.fixture(scope="function")
 def reset_lds_objects(request):
     return request.param()
@@ -16,7 +56,7 @@ def reset_lds_objects(request):
 @pytest.fixture(scope="function")
 def add_lds_objects(reset_lds_objects):
     lds_objects = reset_lds_objects
-    with Session(test_engine) as session:
+    with Session(get_test_engine()) as session:
         for lds_objects_list in lds_objects:
             session.add_all(lds_objects_list)
             session.commit()
