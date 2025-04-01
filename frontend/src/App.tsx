d6d09d536b1c2e882193d7d494a5af48ffefce4c
Nafai7 <nafailuet@gmail.com>
2025-04-01 18:54:00 +0200

    Cleared current frontend work to fresh start and migrated to vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,12 @@
-import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {Appbar} from "./layouts/Appbar";
-import DrawerContainer from "./components/drawer/DrawerContainer";
-import EventTab from "./pages/EventTab";
-import EventDefTab from "./pages/EventDefTab";
-import TrendTab from "./pages/TrendTab";
-import TrendChartTab from "./pages/TrendChartTab";
+import React from 'react'
+import './App.scss'
 
-const App = () => {
-    const [expanded, setExpanded] = React.useState(true);
+function App() {
 
-    const handleDrawerOpen = React.useCallback(() => {
-            setExpanded(!expanded);
-        }, [expanded]
-    );
+  return (
+    <>
+    </>
+  )
+}
 
-    return (
-        <BrowserRouter>
-            <div className="app-container">
-                <Appbar onMenuClick={handleDrawerOpen}/>
-                <DrawerContainer expanded={expanded}>
-                    <Routes>
-                        <Route path="/event" element={<EventTab/>}/>
-                        <Route path="/eventdef" element={<EventDefTab/>}/>
-                        <Route path="/trend" element={<TrendTab/>}/>
-                        <Route path="/trendChart" element={<TrendChartTab/>}/>
-                    </Routes>
-                </DrawerContainer>
-            </div>
-        </BrowserRouter>
-    );
-};
-
-export default App;
+export default App
