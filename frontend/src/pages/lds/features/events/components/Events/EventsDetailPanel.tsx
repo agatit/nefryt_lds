ccf46f4e1278d2173c8e63badcdbb7dda4abc5c2
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2026-02-18 14:31:09 +0100

    refactor: remove ID field and reorder remaining fields

--- frontend/src/pages/lds/features/events/components/Events/EventsDetailPanel.tsx
+++ frontend/src/pages/lds/features/events/components/Events/EventsDetailPanel.tsx
@@ -18,40 +18,13 @@ const EventsDetailPanel = React.memo(function EventsDetailPanel({
       <div className="item">
         <div className="item-column">
           <div>
-            <Label>{t("events-page:id")}</Label>
-            <TextBox value={String(selected.ID)} disabled />
+            <Label>{t("events-page:event_def_id")}</Label>
+            <TextBox value={String(selected.EventDefID)} disabled />
           </div>
-
           <div>
             <Label>{t("events-page:method")}</Label>
             <TextBox value={String(selected.MethodID ?? "")} disabled />
           </div>
-
-          <div>
-            <Label>{t("events-page:details")}</Label>
-            <TextBox value={selected.Details ?? ""} disabled />
-          </div>
-
-          <div>
-            <Label>{t("events-page:position")}</Label>
-            <TextBox value={String(selected.Position ?? "")} disabled />
-          </div>
-
-          <div>
-            <Label>{t("events-page:verbosity")}</Label>
-            <TextBox value={selected.Verbosity ?? ""} disabled />
-          </div>
-
-          <div>
-            <Label>{t("events-page:caption")}</Label>
-            <TextBox value={selected.Caption ?? ""} disabled />
-          </div>
-
-          <div>
-            <Label>{t("events-page:silent")}</Label>
-            <TextBox value={selected.Silent ? "Yes" : "No"} disabled />
-          </div>
-
           <div>
             <Label>{t("events-page:begin_date")}</Label>
             <TextBox
@@ -63,7 +36,6 @@ const EventsDetailPanel = React.memo(function EventsDetailPanel({
               disabled
             />
           </div>
-
           <div>
             <Label>{t("events-page:ack_date")}</Label>
             <TextBox
@@ -75,7 +47,6 @@ const EventsDetailPanel = React.memo(function EventsDetailPanel({
               disabled
             />
           </div>
-
           <div>
             <Label>{t("events-page:end_date")}</Label>
             <TextBox
@@ -83,6 +54,31 @@ const EventsDetailPanel = React.memo(function EventsDetailPanel({
                 selected.EndDate
                   ? new Date(selected.EndDate).toLocaleString()
                   : ""
+              }
+              disabled
+            />
+          </div>
+          <div>
+            <Label>{t("events-page:details")}</Label>
+            <TextBox value={selected.Details ?? ""} disabled />
+          </div>
+          <div>
+            <Label>{t("events-page:position")}</Label>
+            <TextBox value={String(selected.Position ?? "")} disabled />
+          </div>
+          <div>
+            <Label>{t("events-page:verbosity")}</Label>
+            <TextBox value={selected.Verbosity ?? ""} disabled />
+          </div>
+          <div>
+            <Label>{t("events-page:caption")}</Label>
+            <TextBox value={selected.Caption ?? ""} disabled />
+          </div>
+          <div>
+            <Label>{t("events-page:silent")}</Label>
+            <TextBox
+              value={
+                selected.Silent ? t("events-page:yes") : t("events-page:no")
               }
               disabled
             />
